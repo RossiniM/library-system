1ec238fe625faf9dd08eccf3e82c60d4d864bf49
Rossini <rossini1408@gmail.com>
2019-12-12 07:43:04 -0300

    create file to routes and add axios,cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 
-app.get('./',(req,res) =>{
+app.use(cors())
+app.use(routes)
 
-    res.send('teste')
-});
 app.listen(3000,()=> {console.log('listen') })
