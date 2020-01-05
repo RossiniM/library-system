e1c1d777b8b392238599ec02590a199d624f7855
Rossini <rossini1408@gmail.com>
2020-01-05 11:34:14 -0300

    create routes for saving book and use bodyParser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import express from 'express'
-import routes from './routes'
-import cors from 'cors'
+import express from "express";
+import routes from "./routes";
+import Database from "./database/database";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(routes)
-
-app.listen(3000,()=> {console.log('listen') })
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+const port: number = process.env.PORT | 3000;
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(cors());
+app.use(routes);
+new Database().connect().then(() => {
+  app.listen(port, () => console.log(`listen at ${port}`));
+});
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
-import express from 'express'
+import express from "express";
+import BookController from "./controller/book.controller";
 
 const routes = express.Router();
 
+routes.get("/", (req, res) => {
+  res.status(200).json("sucess");
+});
 
-routes.get('/',(req,res) =>{
-    res.status(500).json("sucess")
-})
+routes.post("/book/save", (req, res) => {
+  return new BookController().save(req, res);
+});
 
-export default routes
+export default routes;
